751b0d9d132e565e06004c4d100a0d0f596d36dd
Mike-95 <shokhasoft@gmail.com>
2022-09-26 12:31:08 +0400

    done some change on fragment_word_quiz layout

--- app/src/main/java/Mikepopup/com/Ui/QuickMathFragment.kt
+++ app/src/main/java/Mikepopup/com/Ui/QuickMathFragment.kt
@@ -35,7 +35,7 @@ class QuickMathFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quick_math, container, false)
+        return inflater.inflate(R.layout.fragment_word_quiz, container, false)
     }
 
     companion object {
